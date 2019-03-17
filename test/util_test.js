905dba3d35cc49302bf91c241e677c7811e353bf
Greg Whiteley <greg.whiteley@gmail.com>
2019-03-17 20:20:45 +1100

    util: rework highlightSimpleMatch to support case insensitivity

--- test/util_test.js
+++ test/util_test.js
@@ -6,3 +6,7 @@ assert.equal('<span class="highlight">a</span>bcd<span class="highlight">a</span
              util.highlightSimpleMatch('abcdabcd', 'a'));
 assert.equal('The r<span class="highlight">ain</span> in Sp<span class="highlight">ain</span> falls m<span class="highlight">ain</span>ly on the pl<span class="highlight">ain</span>.',
              util.highlightSimpleMatch('The rain in Spain falls mainly on the plain.', 'ain'));
+
+// case sensitivity
+assert.equal('<span class="highlight">a</span>bcdA', util.highlightSimpleMatch('abcdA', 'a', true));
+assert.equal('<span class="highlight">a</span>bcd<span class="highlight">A</span>', util.highlightSimpleMatch('abcdA', 'a', false));
--- util.js
+++ util.js
@@ -1,9 +1,16 @@
 // Palaver to allow testing via node.js and import into browser
 (function(exports) {
 
+  function escapeRegExp(str) {
+    return str.replace(/([.*+?^=!:${}()|\[\]\/\\])/g, "\\$1");
+  }
+
   // this only works for dumb straight text matches, not regex
-  exports.highlightSimpleMatch = function(text, match) {
-    return text.split(match).join('<span class="highlight">' + match + '</span>');
+  exports.highlightSimpleMatch = function(text, match, caseSensitive = true) {
+    let opts = 'g';
+    if (!caseSensitive)
+      opts += 'i';
+    return text.replace(new RegExp('('+escapeRegExp(match)+')', opts), '<span class="highlight">$1</span>');
   }
 
   // more palaver
